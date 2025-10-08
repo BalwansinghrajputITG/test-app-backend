655e811ab1fb7b620377e06c1d37258fd359bd5a
balwansinghrajputITG <balwanrajput@itgeeks.com>
2025-10-08 13:27:05 +0530

    add history arr

--- src/model/authModel.js
+++ src/model/authModel.js
@@ -24,6 +24,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    scoreHistory: [{ score: { type: Number }, required: true }],
   },
   {
     timestamps: true,
