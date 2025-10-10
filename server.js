455cccc41777e1b0588f1fb1ab70b2b1c5f0a7c5
ParthsharmaITG <parthsharma@itgeeks.com>
2025-10-10 13:58:39 +0530

    just for pulling

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 
-const app = require("./src/index");
-
-app.listen(3000,()=>{ 
-    
-    console.log("site is started http://localhost:3000");
-})
-
-=======
-const app = require("./src/index");
->>>>>>> 9fbab18a0b8edd98c259d9b509a0b57f016b24cb
+const app = require("./src");
 const mongoConnect = require("./src/db/db");
 const errorHandler = require("./src/middleWare/errorHandler");
 mongoConnect();
@@ -19,8 +9,4 @@ const PORT = 3000;
 app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`server is running http://localhost:${PORT}`);
-<<<<<<< HEAD
-});
-=======
-});
->>>>>>> 9fbab18a0b8edd98c259d9b509a0b57f016b24cb
+})
--- src/index.js
+++ src/index.js
@@ -1,34 +1,4 @@
-<<<<<<< HEAD
 
-const express = require('express');
-
-const {default:mongoose} = require('mongoose');
-
-const DashboardRouter = require('./routes/dashboardRoutes');
-
-require('dotenv').config();
-
-const app = express();
-
-app.use(express.json());
-
-app.get("/",(req,res)=>{
-    res.json({msg:"question get successfully"})
-})
-app.use('/dashboard',DashboardRouter);
-
-mongoose.connect(process.env.DATABASE_URL).then(()=>{
-
-    console.log("DB connected");
-
-}).catch((e)=>{
-
-    console.log(e,"db not connected");
-})
-
-
-=======
->>>>>>> 9fbab18a0b8edd98c259d9b509a0b57f016b24cb
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/authRouter");
