b1e2f17beb1153f43d6832ddf14e14640cf7633e
ManishKumarITG <manishkumar@itgeeks.com>
2025-10-10 13:28:36 +0530

    User update functionality added

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -7,4 +7,6 @@ authRouter.post("/login", authController.loginUser);
 authRouter.post("/logout", authController.logOutUsre);
 authRouter.post("/dashboard", authController.dashboard);
 authRouter.post("/Allusers", authController.UsersFetchingData);
+authRouter.post('/updateuser' , authController.updateUser)
+
 module.exports = authRouter;
